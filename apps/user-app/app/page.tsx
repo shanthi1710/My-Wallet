65840e638bc68411398522bb411ffe6871c1f953
shanthi1710 <shanthinathshedabale17@gmail.com>
2024-07-26 00:28:53 +0530

    Add shadcn/ui

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import { Button } from "../@/components/ui/button"
 
-export default function page() {
+export default function Home() {
   return (
-    <div>page</div>
+    <div>
+      <Button >Click me</Button>
+    </div>
   )
 }
